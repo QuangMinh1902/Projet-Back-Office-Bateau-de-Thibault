a7956b923177faeed1dc6cf03405974bc4906d3c
Quang Minh PHAM <quangminh1575@gmail.com>
2023-10-25 06:42:45 +0200

    update

--- src/app/pages/details-produits/details-produits.component.ts
+++ src/app/pages/details-produits/details-produits.component.ts
@@ -20,11 +20,11 @@ export class DetailsProduitsComponent {
 
   constructor(public productsServices : ProductsService){}
 
-  
+
 
   modifierStock(index: number) {
     let product = this.product[index];
-    
+
     const quantiteModifie = product.nb_modifie;
     if (Math.abs(quantiteModifie) > product.quantity_stock && quantiteModifie < 0){
       alert("La quantité du produit dans le stock n'est pas suffisant");
@@ -39,7 +39,7 @@ export class DetailsProduitsComponent {
 
   modifierDiscount(index : number){
     let product = this.product[index];
-    
+
     const discountModifie = product.discount_modifie;
     if (discountModifie < 0){
       alert("Le discount doit être supérieur à 0");
@@ -57,8 +57,48 @@ export class DetailsProduitsComponent {
     this.modifierDiscount(index);
   }
 
+  checkInput(name: string, index: number) {
+    const input = document.getElementById(name) as HTMLInputElement;
 
-  
+    if (input) {
+      const inputValue = parseInt(input.value, 10);
+
+      const errorTag = document.querySelector(`input[id='${name}'] + p`) as HTMLElement | null;
+      const envoyerButton = document.querySelector(`input[id='${name}'] + p + button`) as HTMLButtonElement | null;
+
+      if (inputValue < 0 && Math.abs(inputValue) > this.product[index].quantity_stock) {
+        if (errorTag) {
+          errorTag.style.color = "red";
+          errorTag.innerHTML = "Le nombre à enlever est plus grand que celui en stock";
+          input.classList.add("red-border");
+        }
+
+        if (envoyerButton) {
+          envoyerButton.disabled = true;
+        }
+      } else {
+        if (errorTag) {
+          errorTag.style.color = "initial";
+          errorTag.innerHTML = "";
+        }
+        input.classList.remove("red-border");
+
+        if (envoyerButton) {
+          envoyerButton.disabled = false;
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
 }
 
 
