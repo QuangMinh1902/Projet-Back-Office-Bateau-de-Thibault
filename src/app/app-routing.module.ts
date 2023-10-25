0c4f7407a6e3fe9bacbfb6df2aa130a9f9f6c6d9
Quang Minh PHAM <quangminh1575@gmail.com>
2023-10-25 15:55:57 +0200

    minh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,17 @@ import { DetailsProduitsComponent } from './pages/details-produits/details-produ
 import { PoissonsComponent } from './pages/poissons/poissons.component';
 import { FruitsdemerComponent } from './pages/fruitsdemer/fruitsdemer.component';
 import { CrustacesComponent } from './pages/crustaces/crustaces.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path:'home', component : HomeComponent},
   { path: 'header', component : Headers},
   { path: 'detailsProduits', component : DetailsProduitsComponent},
-  
+
   { path: 'fruitsdemer', component : FruitsdemerComponent},
   { path: 'crustaces', component : CrustacesComponent},
   { path: 'poissons', component : PoissonsComponent},
+  { path: 'dashboard', component : DashboardComponent},
 ];
 
 @NgModule({
