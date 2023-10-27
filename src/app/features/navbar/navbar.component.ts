a2698aa00cd28db55634970d71c36b2cb426834d
Quang Minh PHAM <quangminh1575@gmail.com>
2023-10-27 02:04:37 +0200

    update

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faFish } from '@fortawesome/free-solid-svg-icons';
+import { faShrimp } from '@fortawesome/free-solid-svg-icons';
+import { faFrog } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +13,15 @@ import * as $ from 'jquery';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  magnify = faMagnifyingGlass
+  house = faHouse;
+  cart = faCartArrowDown;
+  fish = faFish;
+  frog = faFrog;
+  shrimp = faShrimp;
   constructor() {}
 
   ngOnInit(): void {
-   
+
   }
 }
